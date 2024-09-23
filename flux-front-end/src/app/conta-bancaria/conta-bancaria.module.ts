3f30581e6c9575ccf800a6949dac18491e286230
regis <regiscostapedro0705@gmail.com>
2024-09-23 16:20:34 -0300

    Feat: Ajustando alyout de transação e criação de conta bancaria

--- flux-front-end/src/app/conta-bancaria/conta-bancaria.module.ts
+++ flux-front-end/src/app/conta-bancaria/conta-bancaria.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -7,14 +7,32 @@ import { IonicModule } from '@ionic/angular';
 import { ContaBancariaPageRoutingModule } from './conta-bancaria-routing.module';
 
 import { ContaBancariaPage } from './conta-bancaria.page';
+import { CURRENCY_MASK_CONFIG, CurrencyMaskConfig, CurrencyMaskModule } from 'ng2-currency-mask';
+
+export const customCurrencyMaskConfig:CurrencyMaskConfig = {
+  align: 'center',
+  allowNegative: false,
+  decimal: ',',
+  precision: 2,
+  prefix: 'R$ ',
+  suffix: '',
+  thousands: '.',
+
+}
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ContaBancariaPageRoutingModule
+    ContaBancariaPageRoutingModule,
+    CurrencyMaskModule,
+
   ],
+  providers: [
+    { provide: CURRENCY_MASK_CONFIG, useValue: customCurrencyMaskConfig },
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [ContaBancariaPage]
 })
 export class ContaBancariaPageModule {}
--- flux-front-end/src/app/conta-bancaria/conta-bancaria.page.ts
+++ flux-front-end/src/app/conta-bancaria/conta-bancaria.page.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { ContaBancariaService } from 'src/app/services/conta-bancaria.service';
-import { AuthService } from 'src/app/services/auth.service'; 
+import { AuthService } from 'src/app/services/auth.service';
 import { ToastController } from '@ionic/angular';
 import { Router } from '@angular/router';
-  import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { IonicSlides } from '@ionic/angular';
+import { ChangeDetectorRef } from '@angular/core';
 interface Banco {
-  id_banco:number;
+  id_banco: number;
   id: number;
   name: string;
   image: string;
-  
+
 }
 
 interface Conta {
@@ -27,11 +29,16 @@ interface Conta {
 })
 export class ContaBancariaPage implements OnInit {
   valor: number = 0;
-  tipoConta: string = ''; 
+  tipoConta: string = '';
   selectedInstitution: number | null = null;
   bancos: Banco[] = [];
   token: string | null = null;
 
+  slideOpts = {
+    initialSlide: 0,
+    slidesPerView: 3,
+    loop: true,
+  };
 
 
   // No construtor, adicione o ActivatedRoute
@@ -40,22 +47,24 @@ export class ContaBancariaPage implements OnInit {
     private authService: AuthService,
     private toastController: ToastController,
     private router: Router,
+    private cdr: ChangeDetectorRef,
+
     private route: ActivatedRoute // Adicionado ActivatedRoute
-  ) {}
-  
+  ) { }
+
   ngOnInit() {
-    this.token = this.authService.getToken();  
+    this.token = this.authService.getToken();
     if (this.token) {
       this.listarInstituicoes();
     } else {
       console.error('Token não encontrado.');
     }
-  
- 
+
+
     this.route.paramMap.subscribe(params => {
-      const bancoId = params.get('id'); 
+      const bancoId = params.get('id');
       if (bancoId) {
-        this.selectedInstitution = +bancoId; 
+        this.selectedInstitution = +bancoId;
         console.log('Banco selecionado via URL:', this.selectedInstitution);
       }
     });
@@ -75,8 +84,8 @@ export class ContaBancariaPage implements OnInit {
     if (this.token) {
       this.contaBancariaService.getInstituicoes(this.token)
         .then((response: Banco[]) => {
-          console.log('Resposta da API:', response); 
-          this.bancos = response; 
+          console.log('Resposta da API:', response);
+          this.bancos = response;
         })
         .catch((err) => {
           console.error('Erro ao listar instituições:', err);
@@ -86,48 +95,49 @@ export class ContaBancariaPage implements OnInit {
 
   selecionarBanco(banco: Banco) {
     this.selectedInstitution = banco.id_banco;
+    this.cdr.detectChanges()
     console.log(this.selectedInstitution)
   }
 
   get bancosFiltrados() {
-    return this.bancos.filter(banco => ['Sicredi', 'Banco do Brasil','Santander','Bradesco','Itaú','Sicoob','Banco Inter','Nu Pagamentos S.A.'].includes(banco.name));
+    return this.bancos.filter(banco => ['Sicredi', 'Banco do Brasil', 'Santander', 'Bradesco', 'Itaú', 'Sicoob', 'Banco Inter', 'Nu Pagamentos S.A.'].includes(banco.name));
   }
 
   async presentToast(message: string) {
     const toast = await this.toastController.create({
       message: message,
-      duration: 5000, 
+      duration: 5000,
       position: 'bottom', // Posição do toast (top, bottom, middle)
       cssClass: 'toast-container'
     });
     toast.present();
   }
-  
+
 
   cadastrarConta() {
     if (this.valor > 0 && this.tipoConta && this.selectedInstitution !== null && this.token) {
-  
-      
-        const conta: Conta = {
-          fkBancoId: this.selectedInstitution,
-          saldo: this.valor,
-          tipo_conta: this.tipoConta 
-        };
 
-        console.log(this.selectedInstitution)
-        console.log(typeof this.tipoConta); 
 
-        console.log('Dados da conta:', conta);
+      const conta: Conta = {
+        fkBancoId: this.selectedInstitution,
+        saldo: this.valor,
+        tipo_conta: this.tipoConta
+      };
 
-        this.contaBancariaService.cadastrarConta(this.token, conta)
-          .then(() => {
-            this.presentToast('Conta Cadastrada com sucesso!');
-            console.log('Conta Bancária cadastrada com sucesso!');
-            this.router.navigate(['/cadastro-banco']);
-          })
-          .catch((err: any) => {
-            console.error('Erro ao cadastrar conta:', err);
-          });
-      } 
-    } 
+      console.log(this.selectedInstitution)
+      console.log(typeof this.tipoConta);
+
+      console.log('Dados da conta:', conta);
+
+      this.contaBancariaService.cadastrarConta(this.token, conta)
+        .then(() => {
+          this.presentToast('Conta Cadastrada com sucesso!');
+          console.log('Conta Bancária cadastrada com sucesso!');
+          this.router.navigate(['/cadastro-banco']);
+        })
+        .catch((err: any) => {
+          console.error('Erro ao cadastrar conta:', err);
+        });
+    }
+  }
 }
