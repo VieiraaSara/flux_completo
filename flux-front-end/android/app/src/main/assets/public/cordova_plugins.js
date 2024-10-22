2245ff7b36ef0faa3736606ca3640f5c555d4b54
regis <regiscostapedro0705@gmail.com>
2024-10-22 20:48:51 -0300

    Fix: Ajustando dependências.

---
+++ flux-front-end/android/app/src/main/assets/public/cordova_plugins.js
@@ -0,0 +1,27 @@
+
+  cordova.define('cordova/plugin_list', function(require, exports, module) {
+    module.exports = [
+      {
+          "id": "cordova-plugin-x-socialsharing.SocialSharing",
+          "file": "plugins/cordova-plugin-x-socialsharing/www/SocialSharing.js",
+          "pluginId": "cordova-plugin-x-socialsharing",
+        "clobbers": [
+          "window.plugins.socialsharing"
+        ]
+        },
+      {
+          "id": "es6-promise-plugin.Promise",
+          "file": "plugins/es6-promise-plugin/www/promise.js",
+          "pluginId": "es6-promise-plugin",
+        "runs": true
+        }
+    ];
+    module.exports.metadata =
+    // TOP OF METADATA
+    {
+      "cordova-plugin-x-socialsharing": "6.0.4",
+      "es6-promise-plugin": "4.2.2"
+    };
+    // BOTTOM OF METADATA
+    });
+    
